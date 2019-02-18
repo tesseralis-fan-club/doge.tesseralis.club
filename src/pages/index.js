339d67cb4240e4e2f2d16986e3f5b4a8b0afa818
Nat Alison <tesseralis@gmail.com>
2019-02-17 17:16:31 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,15 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Official @tesseralis fan site</h1>
-    <p>Welcome to the official tesseralis fan club.</p>
-    <p>Praise me.</p>
-    <p>Here you can praise the beautiful, talented genius that is tesseralis</p>
+    <h1>Much @tesseralis. very official. wow</h1>
+    <p>o hai. very fan club.</p>
+    <p>very praise.</p>
+    <p>So pretty. wow. Much talented.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/page-3/">Go to page 3</Link>
+    <Link to="/page-2/">go 2</Link>
+    <Link to="/page-3/">go 3</Link>
   </Layout>
 )
 
