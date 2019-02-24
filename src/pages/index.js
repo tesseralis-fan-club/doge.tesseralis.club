3d48e5f25ab346dc045ee673d62b369ae0796d52
Nat Alison <tesseralis@gmail.com>
2019-02-24 14:32:03 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,11 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-<<<<<<< HEAD
-    <SEO title="Home" keywords={[`awesome`, `nat`, `tess`]} />
+    <SEO title="Home" keywords={['nat', 'tess', 'alison']} />
     <h1>Much @tesseralis. very official. wow</h1>
     <p>o hai. very fan club.</p>
     <p>very praise.</p>
     <p>So pretty. wow. Much talented.</p>
-=======
-    <SEO title="Home" keywords={['nat', 'tess', 'alison']} />
-    <h1>Official @tesseralis fan site</h1>
-    <p>Welcome to the official tesseralis fan club.</p>
-    <p>Here you can praise the beautiful, talented genius that is tesseralis</p>
->>>>>>> 623f3a02e4e69cb565d8e5d023e65a1ca7822634
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
